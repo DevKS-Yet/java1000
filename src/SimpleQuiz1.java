a6c27d50d0ff205c3cf811bdfa1c4e5ed03956c3
devks-yet <ksang_v@naver.com>
2022-04-08 15:36:45 +0900

    SimpleQuiz1

--- src/SimpleQuiz1.java
+++ src/SimpleQuiz1.java
@@ -7,7 +7,7 @@ class SimpleQuiz1 {
     public static void main(String[] args) {
         String[] data = {
                 "다음 중 키워드가 아닌 것은?`2`final`True`if`public",
-                "다음 중 자바의 연산자가 아닌 것은?`5`&`|`++`!=`/`^",
+                "다음 중 자바의 연산자가 아닌 것은?`6`&`|`++`!=`/`^",
                 "다음 중 메서드의 반환값이 없음을 의미하는 키워드는?`1`void`null`false",
         };
 
