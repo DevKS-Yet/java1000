26d5ea104ab97b253280ad2bd4127c200da9df44
DevKS-Yet <ksang0324@gmail.com>
2022-05-11 22:54:51 +0900

    baekjoon

---
+++ src/BaekJoon0004.java
@@ -0,0 +1,12 @@
+// 10926
+
+import java.util.Scanner;
+
+public class BaekJoon0004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        System.out.println(a + "??!");
+    }
+}
